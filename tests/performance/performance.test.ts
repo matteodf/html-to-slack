8fc43796b8cf058f8be0edc37ef06c859941e586
matteodf <58430487+matteodf@users.noreply.github.com>
2024-06-25 18:47:30 +0200

    test: add performance test for linearizeLists

--- tests/performance/performance.test.ts
+++ tests/performance/performance.test.ts
@@ -1,4 +1,5 @@
 import convertHtmlToSlackBlocks from '../../src/index';
+import { linearizeLists } from '../../src/utils';
 
 describe('Performance Tests', () => {
   // it('should handle large HTML input efficiently', () => {
@@ -81,6 +82,102 @@ describe('Performance Tests', () => {
     const endTime = performance.now();
 
     const executionTime = endTime - startTime;
+
+    // Verify that the conversion was successful
+    expect(blocks.length).toBeGreaterThan(0);
+
+    // Ensure the execution time is within acceptable limits (e.g., less than 5 seconds)
+    expect(executionTime).toBeLessThan(5000);
+  });
+  it('should handle nested lists efficiently', () => {
+    const largeHtml =
+      `<ul>` +
+      `<li>Item 1
+            <ul>
+                <li>Subitem 1.1
+                    <ul>
+                        <li>Subitem 1.1.1
+                            <ul>
+                                <li>Subitem 1.1.1.1</li>
+                                <li>Subitem 1.1.1.2
+                                    <ul>
+                                        <li>Subitem 1.1.1.2.1</li>
+                                        <li>Subitem 1.1.1.2.2
+                                            <ul>
+                                                <li>Subitem 1.1.1.2.2.1</li>
+                                                <li>Subitem 1.1.1.2.2.2</li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>Subitem 1.1.2</li>
+                    </ul>
+                </li>
+                <li>Subitem 1.2</li>
+            </ul>
+        </li>
+        <li>Item 2
+            <ol>
+                <li>Subitem 2.1
+                    <ol>
+                        <li>Subitem 2.1.1
+                            <ol>
+                                <li>Subitem 2.1.1.1</li>
+                                <li>Subitem 2.1.1.2</li>
+                            </ol>
+                        </li>
+                        <li>Subitem 2.1.2</li>
+                    </ol>
+                </li>
+                <li>Subitem 2.2</li>
+            </ol>
+        </li>
+        <li>Item 3
+            <ul>
+                <li>Subitem 3.1
+                    <ul>
+                        <li>Subitem 3.1.1</li>
+                        <li>Subitem 3.1.2</li>
+                    </ul>
+                </li>
+                <li>Subitem 3.2</li>
+            </ul>
+        </li>
+        <li>Item 4
+            <ol>
+                <li>Subitem 4.1
+                    <ol>
+                        <li>Subitem 4.1.1</li>
+                        <li>Subitem 4.1.2</li>
+                    </ol>
+                </li>
+                <li>Subitem 4.2</li>
+            </ol>
+        </li>
+        <li>Item 5
+            <ul>
+                <li>Subitem 5.1
+                    <ul>
+                        <li>Subitem 5.1.1</li>
+                        <li>Subitem 5.1.2</li>
+                    </ul>
+                </li>
+                <li>Subitem 5.2</li>
+            </ul>
+        </li>`.repeat(10000) +
+      `
+    </ul>
+        `;
+
+    const startTime = performance.now();
+    const blocks = linearizeLists(largeHtml);
+    const endTime = performance.now();
+
+    const executionTime = endTime - startTime;
+
+    console.log(executionTime);
 
     // Verify that the conversion was successful
     expect(blocks.length).toBeGreaterThan(0);
